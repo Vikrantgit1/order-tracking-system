bbd68a719784a0303d9aae5b386717cc263f4759
Vikrantgit1 <vikrant051197@gmail.com>
2025-08-16 07:16:49 +0530

    updated OpenApiConfig

--- tracking-service/src/main/java/com/vg/tracking/config/OpenApiConfig.java
+++ tracking-service/src/main/java/com/vg/tracking/config/OpenApiConfig.java
@@ -1,7 +1,9 @@
 package com.vg.tracking.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +13,16 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI().info(
                 new Info()
-                        .title("Order Tracking System - Tracking APIs")
+                        .title("Order Tracking System API")
                         .version("1.0.0")
-                        .description("API documentation for the Order Tracking System")
-        );
+                        .description("Microservices-based real-time order tracking platform")
+                        .termsOfService("https://example.com/terms")
+                        .contact(new Contact()
+                                .name("Vikrant Goyal")
+                                .email("vikrant051197@gmail.com")
+                                .url("https://www.linkedin.com/in/vikrantgoyal/"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")));
     }
 }
